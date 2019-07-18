2b3ff9785ed3735ca2bca44b28ab575a78f6ed38
Morishita1 <iakwpi@naver.com>
2019-07-18 16:49:07 +0900

    first

---
+++ src/main/java/com/biz/memo/dao/MemoDao.java
@@ -0,0 +1,13 @@
+package com.biz.memo.dao;
+
+import java.util.List;
+
+import com.biz.memo.model.MemoVO;
+
+public interface MemoDao {
+	
+	public List<MemoVO> selectAll();
+	public int insert(MemoVO memoVO);
+	public MemoVO findBySeq(Long mo_seq);
+	public int update(MemoVO memoVO);
+}
